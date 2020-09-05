c67c98f7fd6e1c5567284c198a999106d413c08d
Anthony Gregis <anthony@thedevtron.com>
2020-09-05 14:07:49 -0500

    [Boilerplate] Adds Apollo Client, Router, Switch, and Materialize

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from "react"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
+import "../styles/App.css"
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>We are ready to go!</h1>
+      </header>
+      <Switch>
+        {/* Routes to go here later */}
+      </Switch>
+    </div>
+  )
+}
+
+export default App

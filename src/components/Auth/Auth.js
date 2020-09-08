0c1398c79802f477113bcd9a374f0090bdade6d7
subrataroy321 <subrata.r321@gmail.com>
2020-09-08 14:34:53 -0400

    [auth/auth.js] add buttons to render login and register component

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,13 +1,33 @@
-import React from "react"
+import React,{useState} from "react"
 import Login from "./Login"
 import Register from "./Register"
+import {Redirect} from 'react-router-dom'
 import "../../styles/Auth.css"
 
 const Auth = (props) => {
+	let [toggleLogin, setToggleLogin] = useState(true);
+	
+	const handleLoginButton = () => { 
+		setToggleLogin(true)
+	}
+
+	const handleRegisterButton = () => { 
+		setToggleLogin(false)
+	}
+
+	if (props.user) return <Redirect to="/" user={props.user}/> 
+
 	return (
 		<div className="auth-page">
+			<div class="btn-group">
+				<button type="button" class="btn btn-primary" onClick={handleLoginButton}>Login</button>
+				<button type="button" class="btn btn-primary" onClick={handleRegisterButton}>Register</button>
+			</div>
+			{toggleLogin 
+			? 
 			<Login nowCurrentUser={props.nowCurrentUser} user={props.user}/>
-			<Register/>
+			:
+			<Register/>}
 		</div>
 	)
 }

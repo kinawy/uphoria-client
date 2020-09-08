d407ed8f7b3705946485f7a27d44bd3a2901fa8c
subrataroy321 <subrata.r321@gmail.com>
2020-09-08 15:38:39 -0400

    [features/logout] fix redirect for logout, moved logout button to profile page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,12 +1,18 @@
 import React from "react"
 import VideoGrid from "./VideoGrid"
 import UserInfo from "./UserInfo"
+import {Redirect} from 'react-router-dom'
+import Logout from "../Logout/Logout"
 import "../../styles/Profile.css"
 
 const Profile = (props) => {
 	console.log("Profile Prop:", props.user)
+
+	if (!props.user) return <Redirect to="/auth"/> 
+
 	return (
 		<div className="profile-display">
+			<Logout handleLogout={props.handleLogout}/>
 			<UserInfo user={props.user}/>
 			<VideoGrid/>
 		</div>

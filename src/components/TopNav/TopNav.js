c29743727fd17ed40aa8e1eba36b1aafefe5e2fe
Anthony Gregis <anthony@thedevtron.com>
2020-09-09 13:09:14 -0500

    [Video Page] Video stacking and top nav transparent

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -5,6 +5,7 @@ import Tabs from "@material-ui/core/Tabs"
 import Tab from "@material-ui/core/Tab"
 import Typography from "@material-ui/core/Typography"
 import Box from "@material-ui/core/Box"
+import "../../styles/TopNav.css"
 
 const TabPanel = (props) => {
   const { children, value, index, ...other } = props
@@ -37,7 +38,7 @@ const TopNav = (props) => {
 
   return (
     <div className="header-tab">
-      <AppBar position="static" color="default">
+      <AppBar position="static" color="transparent">
         <Tabs
           value={value}
           onChange={handleChange}

4b83f8970a91f77c2bdeeaa8d22b137ff6acfa95
kinawy <sameh.yasser.kinawy@gmail.com>
2020-09-09 16:00:41 -0400

    Almost full functionality for video grid

--- src/components/Profile/VideoGrid.js
+++ src/components/Profile/VideoGrid.js
@@ -1,89 +1,65 @@
 import React from "react"
-import {makeStyles} from "@material-ui/core/styles"
+import { makeStyles } from "@material-ui/core/styles"
 import Grid from "@material-ui/core/Grid"
 
 const useStyles = makeStyles((theme) => ({
   root: {
-	  marginBottom: "5vh",
-	  border: "1px solid black",
+    marginBottom: "5vh",
+    border: "1px solid black",
   },
-	row: {
-  	//Empty CSS
-	},
-	item: {
-  	height: "100%",
-		paddingBottom: "0px",
-	},
+  row: {
+    //Empty CSS
+  },
+  item: {
+    height: "100%",
+    paddingBottom: "0px",
+  },
   video: {
-	  paddingBottom: "0px",
+    paddingBottom: "0px",
     border: "1px solid black",
     textAlign: "center",
-	  height: "100%",
+    height: "100%",
     width: "100%",
     position: "relative",
-  }
+  },
 }))
 
 const VideoGrid = (props) => {
   const classes = useStyles()
   let videos = []
-
-  
+  let newVideos = []
 
   const FormRow = () => {
-    while(videos < props.videos.length) {
-    let getVideos = props.videos.map(video => {
-      videos.push(video)
-      console.log(videos)
-      return videos
-    })
-  }
-
-    return (
-      <React.Fragment>
-        <Grid className={classes.item} item xs={4} p={0} m={0}>
-          <video
-              src={`https://res.cloudinary.com/agregis/video/upload/e_boomerang/eo_1/c_scale,g_south_west,l_logo_transparent_wvrps0,w_144/${videos[0].videoUrl}.mp4`}
+    while (videos < props.videos.length) {
+      props.videos.forEach((video) => {
+        videos.push(video)
+        console.log(videos)
+      })
+    }
+    let i
+    for (i = 0; i < videos.length; i++) {
+      newVideos.push(
+        <Grid className={classes.row} container item s={12} p={0} m={0}>
+          <Grid className={classes.item} item xs={4} p={0} m={0}>
+            <video
+              src={`https://res.cloudinary.com/agregis/video/upload/e_boomerang/eo_1/c_scale,g_south_west,l_logo_transparent_wvrps0,w_144/${videos[i].videoUrl}.mp4`}
               autoPlay={true}
               loop={true}
               muted
               className={classes.video}
             />
+          </Grid>
         </Grid>
-        <Grid className={classes.item} item xs={4} p={0} m={0}>
-          <video
-              src={`https://res.cloudinary.com/agregis/video/upload/e_boomerang/eo_1/c_scale,g_south_west,l_logo_transparent_wvrps0,w_144/${videos[1].videoUrl}.mp4`}
-              autoPlay={true}
-              loop={true}
-              muted
-              className={classes.video}
-            />
-        </Grid>
-        <Grid className={classes.item} item xs={4} p={0} m={0}>
-          <video
-              src={`https://res.cloudinary.com/agregis/video/upload/e_boomerang/eo_1/c_scale,g_south_west,l_logo_transparent_wvrps0,w_144/${videos[1].videoUrl}.mp4`}
-              autoPlay={true}
-              loop={true}
-              muted
-              className={classes.video}
-            />
-        </Grid>
-      </React.Fragment>
-    )
+      )
+      console.log(newVideos)
+    }
+    return newVideos
   }
 
   return (
     <div className={classes.root}>
       <Grid container>
-        <Grid className={classes.row} container item s={12} p={0} m={0}>
-          <FormRow/>
-        </Grid>
-        <Grid className={classes.row} container item s={12} p={0} m={0}>
-          <FormRow/>
-        </Grid>
-        <Grid className={classes.row} container item s={12} p={0} m={0}>
-          <FormRow/>
-        </Grid>
+        <FormRow />
       </Grid>
     </div>
   )

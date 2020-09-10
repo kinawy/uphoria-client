6495b778a55dd9a853f5bcec7adb3f03177dcd38
kinawy <sameh.yasser.kinawy@gmail.com>
2020-09-10 19:26:36 -0400

    Fixes route in Link for Logout Button

--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 const Logout = (props) => {
   return (
-    <Link className="btn btn-primary logoutButton" onClick={props.handleLogout}>
+    <Link className="btn btn-primary logoutButton" onClick={props.handleLogout} to="/auth">
       Log-out
     </Link>
   )

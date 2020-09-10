5a12642920806d1c2fb5c6003fa887601826d7a0
kinawy <sameh.yasser.kinawy@gmail.com>
2020-09-09 20:12:25 -0400

    Adds key to video js, bugs freak me out

--- src/components/Video.js
+++ src/components/Video.js
@@ -36,7 +36,7 @@ const Video = (props) => {
 
           let videos = data.videos.map((video) => {
             return (
-              <div className="Video-Container">
+              <div className="Video-Container" key={video.id}>
                 <SideBar
 	                videoId={video.id}
                   userId={video.userId.id}

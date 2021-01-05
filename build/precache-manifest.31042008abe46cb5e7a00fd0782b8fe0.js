8b284154e04388aa0b370b9785c3c9e8574b23b4
kinawy <sameh.yasser.kinawy@gmail.com>
2021-01-05 19:34:02 +0200

    Adds buildpack

---
+++ build/precache-manifest.31042008abe46cb5e7a00fd0782b8fe0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7effacd3075e7ac69facb147d4579d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e5ab3a7b2bebdd57755",
+    "url": "/static/css/main.6815e7c8.chunk.css"
+  },
+  {
+    "revision": "78ce69c08761153c8d33",
+    "url": "/static/js/2.35624fc0.chunk.js"
+  },
+  {
+    "revision": "66635ff47d5db0e925fcfbc70af736c8",
+    "url": "/static/js/2.35624fc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e5ab3a7b2bebdd57755",
+    "url": "/static/js/main.4f1a1b61.chunk.js"
+  },
+  {
+    "revision": "57da08007b38c9fe8b79",
+    "url": "/static/js/runtime-main.41a1894c.js"
+  },
+  {
+    "revision": "46ce74c2e7da57b99857891a9e0ebf94",
+    "url": "/static/media/instagram.46ce74c2.svg"
+  },
+  {
+    "revision": "262c1a29c484a7015f09c4617a5b3921",
+    "url": "/static/media/link.262c1a29.svg"
+  },
+  {
+    "revision": "d219630e9d84afe9c4f70faffdf81961",
+    "url": "/static/media/logo.d219630e.png"
+  },
+  {
+    "revision": "4da78c22a4c8e16a827e241a5b0cfc8a",
+    "url": "/static/media/ulogo.4da78c22.png"
+  },
+  {
+    "revision": "eedb3ea7c4e59c1c6cd6fe7a881fbb2c",
+    "url": "/static/media/unmute.eedb3ea7.png"
+  }
+]);
